51fb47c54e998f22d1fbac6681c96f49f4e765bd
kuminkyu9 <minkugu@naver.com>
2026-01-29 22:35:37 +0900

    Initial commit

---
+++ src/main/java/com/github/kuminkyu9/limiteddropserver/LimitedDropServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kuminkyu9.limiteddropserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedDropServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedDropServerApplication.class, args);
+	}
+
+}
